cc998d9c9b34e0f7e856c781b58553bdb901f601
Joseph Utecht <joseph.utecht@gmail.com>
2020-02-24 13:25:17 -0600

    transform work

--- features/step_definitions/transform_steps.rb
+++ features/step_definitions/transform_steps.rb
@@ -33,18 +33,18 @@ Then('inv * v = vector\({float}, {float}, {float})') do |x, y, z|
   expect(@inv * @v).to eq Vector.new(x, y, z)
 end
 
-Given('half_quarter ← rotation_x\(π \/ {float})') do |float|
-  @half_quarter = Transform.rotation_x(Math.PI / float)
+Given('half_quarter ← rotation_x\(π / {float})') do |float|
+  @half_quarter = Transform.rotation_x(Math::PI / float)
 end
 
-Given('full_quarter ← rotation_x\(π \/ {float})') do |float|
-  @full_quarter = Transform.rotation_x(Math.PI / float)
+Given('full_quarter ← rotation_x\(π / {float})') do |float|
+  @full_quarter = Transform.rotation_x(Math::PI / float)
 end
 
 Then('half_quarter * p = point\({int}, √2\/{int}, √2\/{int})') do |int, int2, int3|
   x = int
-  y = Math.sqrt(2 / int2)
-  z = Math.sqrt(2 / int3)
+  y = Math.sqrt(2) / int2
+  z = Math.sqrt(2) / int3
   expect(@half_quarter * @p).to eq Point.new(x, y, z)
 end
 
@@ -58,7 +58,86 @@ end
 
 Then('inv * p = point\({int}, √2\/{int}, -√2\/{int})') do |int, int2, int3|
   x = int
-  y = Math.sqrt(2 / int2)
-  z = Math.sqrt(2 / int3) * -1
+  y = Math.sqrt(2) / int2
+  z = -Math.sqrt(2) / int3
   expect(@inv * @p).to eq Point.new(x, y, z)
+end
+
+Given('half_quarter ← rotation_y\(π / {float})') do |float|
+  @half_quarter = Transform.rotation_y(Math::PI / float)
+end
+
+Given('full_quarter ← rotation_y\(π / {float})') do |float|
+  @full_quarter = Transform.rotation_y(Math::PI / float)
+end
+
+Then('half_quarter * p = point\(√2\/{int}, {int}, √2\/{int})') do |int, int2, int3|
+  x = Math.sqrt(2) / int
+  y = int2
+  z = Math.sqrt(2) / int3
+  expect(@half_quarter * @p).to eq Point.new(x, y, z)
+end
+
+
+Given('half_quarter ← rotation_z\(π / {float})') do |float|
+  @half_quarter = Transform.rotation_z(Math::PI / float)
+end
+
+Given('full_quarter ← rotation_z\(π / {float})') do |float|
+  @full_quarter = Transform.rotation_z(Math::PI / float)
+end
+
+Then('half_quarter * p = point\(-√2\/{int}, √2\/{int}, {int})') do |int, int2, int3|
+  x = -Math.sqrt(2) / int
+  y = Math.sqrt(2) / int2
+  z = int3
+  expect(@half_quarter * @p).to eq Point.new(x, y, z)
+end
+
+Given('transform ← shearing\({float}, {float}, {float}, {float}, {float}, {float})') do |float, float2, float3, float4, float5, float6|
+  @transform = Transform.shearing(float, float2, float3, float4, float5, float6)
+end
+
+Given('A ← rotation_x\(π / {float})') do |float|
+  @A = Transform.rotation_x(Math::PI / float)
+end
+
+Given('B ← scaling\({float}, {float}, {float})') do |x, y, z|
+  @B = Transform.scaling(x, y, z)
+end
+
+Given('C ← translation\({float}, {float}, {float})') do |x, y, z|
+  @C = Transform.translation(x, y, z)
+end
+
+When('p2 ← A * p') do
+  @p2 = @A * @p
+end
+
+Then('p2 = point\({float}, {float}, {float})') do |x, y, z|
+  expect(@p2).to eq Point.new(x, y, z)
+end
+
+When('p3 ← B * p2') do
+  @p3 = @B * @p2
+end
+
+Then('p3 = point\({float}, {float}, {float})') do |x, y, z|
+  expect(@p3).to eq Point.new(x, y, z)
+end
+
+Then('p4 = point\({float}, {float}, {float})') do |x, y, z|
+  expect(@p4).to eq Point.new(x, y, z)
+end
+
+When('T ← C * B * A') do
+  @T = @C * @B * @A
+end
+
+Then('T * p = point\({float}, {float}, {float})') do |x, y, z|
+  expect(@T * @p).to eq Point.new(x, y, z)
+end
+
+When('p4 ← C * p3') do
+  @p4 = @C * @p3
 end
--- lib/transformation.rb
+++ lib/transformation.rb
@@ -24,5 +24,50 @@ class Transform
   end
 
   def self.rotation_x(radians)
+    @matrix = Matrix.new(4)
+    @matrix[0, 0] = 1
+    @matrix[1, 1] = Math.cos(radians)
+    @matrix[2, 1] = Math.sin(radians)
+    @matrix[1, 2] = -Math.sin(radians)
+    @matrix[2, 2] = Math.cos(radians)
+    @matrix[3, 3] = 1
+    return @matrix
+  end
+
+  def self.rotation_y(radians)
+    @matrix = Matrix.new(4)
+    @matrix[0, 0] = Math.cos(radians)
+    @matrix[1, 1] = 1
+    @matrix[0, 2] = Math.sin(radians)
+    @matrix[2, 0] = -Math.sin(radians)
+    @matrix[2, 2] = Math.cos(radians)
+    @matrix[3, 3] = 1
+    return @matrix
+  end
+
+  def self.rotation_z(radians)
+    @matrix = Matrix.new(4)
+    @matrix[0, 0] = Math.cos(radians)
+    @matrix[1, 1] = Math.cos(radians)
+    @matrix[0, 1] = -Math.sin(radians)
+    @matrix[1, 0] = Math.sin(radians)
+    @matrix[2, 2] = 1
+    @matrix[3, 3] = 1
+    return @matrix
+  end
+
+  def self.shearing(xy, xz, yx, yz, zx, zy)
+    @matrix = Matrix.new(4)
+    @matrix[0, 0] = 1
+    @matrix[0, 1] = xy
+    @matrix[1, 0] = yx
+    @matrix[1, 1] = 1
+    @matrix[0, 2] = xz
+    @matrix[2, 0] = zx
+    @matrix[1, 2] = yz
+    @matrix[2, 1] = zy
+    @matrix[2, 2] = 1
+    @matrix[3, 3] = 1
+    return @matrix
   end
 end
